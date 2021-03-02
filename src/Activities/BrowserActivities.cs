61cbb03eb7ef2084c1975e4b55e9b6546edf790b
Ben Hoff <bhoff@joltag.com>
2021-03-02 13:34:44 -0800

    initial commit

---
+++ src/Activities/BrowserActivities.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using UiPath.Core.Activities;
+using System.Activities.Presentation;
+using System.Windows;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace SampleGovernedActivities.Activities
+{
+    public class GovernedOpenBrowser : OpenBrowser
+    {
+        protected List<string> whitelist = new List<string> {"test.com",
+                                                             "www.test.com",
+                                                             ""};
+        protected override Task<object> ExecuteAsync(NativeActivityContext context)
+        {
+            string url = base.Url.Get(context);
+            var uri = new Uri(url);
+            string host = uri.Host;
+
+            if (!(whitelist.Contains(host)))
+            {
+                throw new Exception("Prohibited Url");
+            }
+
+            //If no exception is thrown by the validation check, call the base class's "ExecuteAsync"
+            //which will send the message per the settings
+            return base.ExecuteAsync(context);
+        }
+    }
+
+    public sealed class GovernedOpenBrowserFactory : IActivityTemplateFactory<GovernedOpenBrowser>
+    {
+        public GovernedOpenBrowser Create(DependencyObject target, System.Windows.IDataObject dataObject)
+        {
+            var actv = new GovernedOpenBrowser();
+            return actv;        
+        }
+
+    }
+
+    public class GovernedNavigateTo : NavigateTo
+
+    {
+        protected List<string> whitelist = new List<string> {"test.com",
+                                                             "www.test.com",
+                                                             ""};
+        protected override Task ExecuteAsync(AsyncCodeActivityContext context, CancellationToken cancellationToken)
+        {
+            string url = base.Url.Get(context);
+            var regular_expression = new Regex(url);
+            if (!(whitelist.Where(f => regular_expression.IsMatch(f)).ToList().Any<string>()))
+            {
+                throw new Exception("Prohibited Url");
+            }
+            return base.ExecuteAsync(context, cancellationToken);
+        }
+    }
+}
---
+++ src/MetaDataProvider.cs
@@ -0,0 +1,31 @@
+﻿using SampleGovernedActivities.Activities;
+using System.Activities;
+using System.Activities.Presentation.Metadata;
+using System.ComponentModel;
+
+namespace SampleGovernedActivities
+{
+    //This class registers the activities so they can be found in Studio(X).
+    public class MetaDataProvider : IRegisterMetadata
+    {
+        public void Register()
+        {
+            var builder = new AttributeTableBuilder();
+
+            // In StudioX, an activity's catgory must start with "Business.". 
+            // In this case, we'll place the "Mail" activities into the "Mail" category since
+            // we want users to use these instead of the included ones
+
+            // Register the activities's categories            
+            builder.AddCustomAttributes(typeof(GovernedNavigateTo), new CategoryAttribute("Business.Browser"));
+            builder.AddCustomAttributes(typeof(GovernedOpenBrowserFactory), new CategoryAttribute("Business.Browser"));
+
+
+            //Set their display names
+            builder.AddCustomAttributes(typeof(GovernedNavigateTo), new DisplayNameAttribute("Governed Navigate to"));
+            builder.AddCustomAttributes(typeof(GovernedOpenBrowser), new DisplayNameAttribute("Governed Open Browser"));
+            builder.AddCustomAttributes(typeof(GovernedOpenBrowserFactory), new DisplayNameAttribute("Governed Open Browser"));
+            MetadataStore.AddAttributeTable(builder.CreateTable());
+        }
+    }
+}
